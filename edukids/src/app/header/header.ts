ed17111b4efc86630ccc80ae8ae39926f6f9d54c
Juan Diego <jroque@frba.utn.edu.ar>
2026-05-16 03:47:32 -0300

    scroll

--- edukids/src/app/header/header.ts
+++ edukids/src/app/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,9 +8,26 @@ import { Component } from '@angular/core';
 })
 export class Header {
   isMenuOpen: boolean = false;
+  isVisible: boolean = true;
+  lastScrollTop: number = 0;
 
   // Función que abre o cierra el menú
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+    
+    // Si scrolleamos hacia abajo y pasamos el área del header
+    if (st > this.lastScrollTop && st > 100) {
+      this.isVisible = false;
+      this.isMenuOpen = false; // Cerramos menú si estaba abierto al bajar
+    } else {
+      // Si scrolleamos hacia arriba
+      this.isVisible = true;
+    }
+    this.lastScrollTop = st <= 0 ? 0 : st;
   }
 }
